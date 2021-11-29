293574f5fbcfd0713ab99bdd2c3866e40e87c957
Nick D <Ndefected15@gmail.com>
2021-11-28 20:42:45 -0500

    almost done

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,14 @@
-var one = document.getElementById("1pm");
-var two = document.getElementById("2pm");
-var three = document.getElementById("3pm");
-var four = document.getElementById("4pm");
-var five = document.getElementById("5pm");
-var nine = document.getElementById("9am");
-var ten = document.getElementById("10am");
-var eleven = document.getElementById("11am");
-var twelve = document.getElementById("12pm");
+var one = $("#1pm");
+var two = $("#2pm");
+var three = $("#3pm");
+var four = $("#4pm");
+var five = $("#5pm");
+var nine = $("#9am");
+var ten = $("#10am");
+var eleven = $("#11am");
+var twelve = $("#12pm");
 
 var date = moment().format("dddd") + " " + moment().format("Do MMM YYYY");
-var hour = moment().format("h:mm:ss a");
 
 var hour = moment().hours();
 var userInput;
@@ -23,51 +22,35 @@ var interval = setInterval(function () {
 });
 
 function start() {
+  localStorage.clear;
   console.log("Current Hour " + hour);
 
-  var init1 = JSON.parse(localStorage.getItem("01:00pm"));
-  if (one) {
-    return init1;
-  }
+  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+  nine.val(init9);
 
-  var init2 = JSON.parse(localStorage.getItem("02:00pm"));
-  if (two) {
-    return init2;
-  }
+  var init10 = JSON.parse(localStorage.getItem("10:00 am"));
+  ten.val(init10);
 
-  var init3 = JSON.parse(localStorage.getItem("03:00pm"));
-  if (three) {
-    return init3;
-  }
+  var init11 = JSON.parse(localStorage.getItem("11:00 am"));
+  eleven.val(init11);
 
-  var init4 = JSON.parse(localStorage.getItem("04:00pm"));
-  if (four) {
-    return init4;
-  }
+  var init12 = JSON.parse(localStorage.getItem("12:00 pm"));
+  twelve.val(init12);
 
-  var init5 = JSON.parse(localStorage.getItem("05:00pm"));
-  if (five) {
-    return init5;
-  }
-  var init9 = JSON.parse(localStorage.getItem("09:00am"));
-  if (nine) {
-    return init9;
-  }
+  var init1 = JSON.parse(localStorage.getItem("01:00 pm"));
+  one.val(init1);
 
-  var init10 = JSON.parse(localStorage.getItem("10:00am"));
-  if (ten) {
-    return init10;
-  }
+  var init2 = JSON.parse(localStorage.getItem("02:00 pm"));
+  two.val(init2);
 
-  var init11 = JSON.parse(localStorage.getItem("11:00am"));
-  if (eleven) {
-    return init11;
-  }
+  var init3 = JSON.parse(localStorage.getItem("03:00 pm"));
+  three.val(init3);
 
-  var init12 = JSON.parse(localStorage.getItem("12:00pm"));
-  if (twelve) {
-    return init12;
-  }
+  var init4 = JSON.parse(localStorage.getItem("04:00 pm"));
+  four.val(init4);
+
+  var init5 = JSON.parse(localStorage.getItem("05:00 pm"));
+  five.val(init5);
 }
 
 function timeCheck() {
@@ -93,9 +76,7 @@ $(document).ready(function () {
 
   $(".saveBtn").on("click", function () {
     userInput = $(this).siblings(".form-control").val();
-    console.log(userInput);
     hourSpan = $(this).siblings(".input-group-prepend").text();
-    console.log(hourSpan);
     localStorage.setItem(hourSpan, JSON.stringify(userInput));
   });
 });
